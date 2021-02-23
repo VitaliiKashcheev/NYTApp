d2d6a4551ac5f51064e42cfbaab0fc9475f30bca
Vitalii Kashcheiev <vitalii.kashcheiev@icloud.com>
2021-02-23 16:37:49 +0200

    Initial Commit

---
+++ NYTApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTApp
+//
+//  Created by Виталий on 2/23/21.
+//  Copyright © 2021 Виталий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
